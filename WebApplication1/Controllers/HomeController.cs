ea41e9e12972693e5b087c66e2d33e4718043394
Alistair Robson <adrobson@hotmail.com>
2016-03-03 11:18:32 +0000

    Client MVC

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -38,5 +38,6 @@ namespace WebApplication1.Controllers
 
             return View(aMazeWithABomb);
         }
+        
     }
 }
---
+++ WebApplication1/Helpers/MyHtmlHelpers.cs
@@ -0,0 +1,57 @@
+﻿using AbstractFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Helpers
+{
+    public class MyHtmlHelpers
+    {
+        public static IHtmlString FormattedRoomDiv(Room room)
+        {
+            string cssString = "col-md-6 " + SetFormatting(room);
+            string htmlString = String.Format("<div class='" + cssString + "'></div>", room);
+            return new HtmlString(htmlString);
+        }
+
+        public static string SetFormatting(Room r)
+        {
+            string format = RoomBackground(r);
+
+            format += BorderFormat(r.Sides.Where(x => x.Key == Direction.North).FirstOrDefault().Value, "top");
+            format += BorderFormat(r.Sides.Where(x => x.Key == Direction.East).FirstOrDefault().Value, "right");
+            format += BorderFormat(r.Sides.Where(x => x.Key == Direction.South).FirstOrDefault().Value, "bottom");
+            format += BorderFormat(r.Sides.Where(x => x.Key == Direction.West).FirstOrDefault().Value, "left");
+            return format;
+        }
+
+        public static string RoomBackground(MapSite m)
+        {
+            if (m.GetType().ToString() == "AbstractFactory.EnchantedRoom")
+            {
+                return " room enchanted";
+            }
+            else if (m.GetType().ToString() == "AbstractFactory.RoomWithABomb")
+            {
+                return " room withABomb";
+            }
+            else
+            {
+                return "room";
+            }
+
+        }
+        public static string BorderFormat(MapSite m, string direction)
+        {
+            if (m.GetType().ToString() == "AbstractFactory.Wall")
+            {
+                return " wall-" + direction;
+            }
+            else
+            {
+                return " door-" + direction;
+            }
+        }
+    }
+}
